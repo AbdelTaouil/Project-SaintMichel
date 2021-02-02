cea5c0fd868959034bf409ee266bbfb34d339ea5
oussamaghzl <oussama.ghzl6@gmail.com>
2021-02-02 15:44:45 +0100

    laravelisation4

--- resources/views/frontend/Accueil.blade.php
+++ resources/views/frontend/Accueil.blade.php
@@ -34,137 +34,8 @@
               </div>
             </header>
 
-            <!-- Banner -->
-            <section class="main-banner">
-              <div class="container-fluid">
-                <div class="row">
-                  <div class="col-md-12">
-                    <div class="banner-content">
-                      <div class="row">
-                        <div class="col-md-12">
-                          <div class="banner-caption">
-                            <h4>Hello, this is your <em>Ramayana</em> Theme.</h4>
-                            <span>AWESOME HTML5 &amp; CSS3 TEMPLATE</span>
-                            <p>Do you know that <strong>Ramayana</strong> is a free to use Bootstrap 4 CSS template by <strong>templatemo</strong>? You can edit and add more pages as you wanted for your personal or corporate websites.</p>
-                            <div class="primary-button">
-                              <a href="#">Read More</a>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
-
-            <!-- Services -->
-            <section class="services">
-              <div class="container-fluid">
-                <div class="row">
-                  <div class="col-md-4">
-                    <div class="service-item first-item">
-                      <div class="icon"></div>
-                      <h4>Web Design</h4>
-                      <p>Credit goes to <a rel="nofollow" href="https://www.pexels.com">Pexels</a> and <a rel="nofollow" href="https://www.rawpixel.com">Raw Pixel</a> for images used in this template. Thank you.</p>
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="service-item second-item">
-                      <div class="icon"></div>
-                      <h4>Bootstrap 4</h4>
-                      <p>Proin aliquam facilisis ante interdum. Sed nulla feugiat tempus aliquam.</p>
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="service-item third-item">
-                      <div class="icon"></div>
-                      <h4>HTML CSS</h4>
-                      <p>Proin aliquam facilisis ante interdum. Sed nulla feugiat tempus aliquam.</p>
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="service-item fourth-item">
-                      <div class="icon"></div>
-                      <h4>Download Free</h4>
-                      <p>We have many free to use CSS web templates on our site for you.</p>
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="service-item fivth-item">
-                      <div class="icon"></div>
-                      <h4>Get in touch</h4>
-                      <p>You can get the fastest response from <a rel="nofollow" href="https://www.facebook.com/templatemo">templatemo</a> facebook page.</p>
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="service-item sixth-item">
-                      <div class="icon"></div>
-                      <h4>Spread a word</h4>
-                      <p>Please tell your friends about our website. This is very helpful.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
-
-            <!-- Top Image -->
-            <section class="top-image">
-              <div class="container-fluid">
-                <div class="row">
-                  <div class="col-md-12">
-                    <img src="assets/images/top-image.jpg" alt="">
-                    <div class="down-content">
-                      <h4>Ante Interdum Chambray</h4>
-                      <p>Lorem ipsum dolor amet raclette chambray bitters, hammock celiac slow-carb flexitarian four dollar toast food truck health goth. Air plant brunch food truck vegan scenester organic crucifix irony pour-over pop-up austin hexagon kitsch swag. Godard literally humblebrag cloud bread vice master cleanse chambray typewriter put a bird on it brooklyn forage.</p>
-                      <div class="primary-button">
-                        <a href="#">Read More</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
-
-            <!-- Left Image -->
-            <section class="left-image">
-              <div class="container-fluid">
-                <div class="row">
-                  <div class="col-md-6">
-                    <img src="assets/images/left-image.jpg" alt="">
-                  </div>
-                  <div class="col-md-6">
-                    <div class="right-content">
-                      <h4>Ante Interdum Raclette</h4>
-                      <p>Lorem ipsum dolor amet raclette chambray bitters, hammock celiac slow-carb flexitarian four dollar toast food truck health goth. Air plant brunch food truck vegan scenester organic crucifix irony pour-over pop-up austin hexagon kitsch swag. Godard literally humblebrag cloud bread vice master cleanse chambray typewriter put a bird on it brooklyn forage.<br><br>Air plant brunch food truck vegan scenester organic crucifix irony pour-over pop-up austin hexagon kitsch swag. Godard literally humblebrag cloud bread vice master cleanse chambray typewriter put bird brooklyn</p>
-                      <div class="primary-button">
-                        <a href="#">Read More</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
-
-            <!-- Right Image -->
-            <section class="right-image">
-              <div class="container-fluid">
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="left-content">
-                      <h4>Ante Interdum Raclette</h4>
-                      <p>Lorem ipsum dolor amet raclette chambray bitters, hammock celiac slow-carb flexitarian four dollar toast food truck health goth. Air plant brunch food truck vegan scenester organic crucifix irony pour-over pop-up austin hexagon kitsch swag. Godard literally humblebrag cloud bread vice master cleanse chambray typewriter put a bird on it brooklyn forage.<br><br>Air plant brunch food truck vegan scenester organic crucifix irony pour-over pop-up austin hexagon kitsch swag. Godard literally humblebrag cloud bread vice master cleanse chambray typewriter put bird brooklyn</p>
-                      <div class="primary-button">
-                        <a href="#">Read More</a>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <img src="assets/images/right-image.jpg" alt="">
-                  </div>
-                </div>
-              </div>
-            </section>
+            
+            
 
           </div>
         </div>
@@ -215,19 +86,19 @@
               <div class="owl-carousel owl-theme">
                 <a href="#">
                   <div class="featured-item">
-                    <img src="assets/images/featured_post_01.jpg" alt="featured one">
+                    <img src="{{asset('images/featured_post_01.jpg')}}" alt="featured one">
                     <p>Aliquam egestas convallis eros sed gravida. Curabitur consequat sit.</p>
                   </div>
                 </a>
                 <a href="#">
                   <div class="featured-item">
-                    <img src="assets/images/featured_post_01.jpg" alt="featured two">
+                    <img src="{{asset('images/featured_post_01.jpg')}}" alt="featured one">
                     <p>Donec a scelerisque massa. Aliquam non iaculis quam. Duis arcu turpis.</p>
                   </div>
                 </a>
                 <a href="#">
                   <div class="featured-item">
-                    <img src="assets/images/featured_post_01.jpg" alt="featured three">
+                    <img src="{{asset('images/featured_post_01.jpg')}}" alt="featured one">
                     <p>Suspendisse ac convallis urna, vitae luctus ante. Donec sit amet.</p>
                   </div>
                 </a>
